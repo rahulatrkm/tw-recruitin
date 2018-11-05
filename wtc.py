f6b01f5eb900ba775e95d0869d70435bde739495
Rahul <rahulatrkm@gmail.com>
2018-11-05 22:43:39 +0530

    who's the closet partially completd with 11 test case passed

--- wtc.py
+++ wtc.py
@@ -16,52 +16,39 @@ import sys
 #  1. STRING s
 #  2. INTEGER_ARRAY queries
 #
+def check(s, q, qc):
+    if s[q] == s[qc]:
+        return True
+    else:
+        return False
 
 def closest(s, queries):
     # Write your code here
     a = []
-    #print(s)
-    #print(queries)
-
     for query in queries:
-        c = s.count(s[query])
-        #print(c)
-        #print(a)
-        if c == 1:
-            a.append(-1)
-        elif (query == 0):
-            i = 1
-            while (s[query] != s[i]):
-                i +=1
-            a.append(i)
-        elif (query == (len(s)-1)):
-            i = (len(s)-2)
-            while (s[query] != s[i]):
+        i = query
+        j = query
+        l = len(s)
+        flag = 0
+        if queries.co
+        while (i != 0) or (j != (l-1)):
+            if i != 0:
                 i -= 1
-            a.append(i)
-        else:
-            if query > 0:
-                i = query - 1
-            else :
-                i = query
-            if query < (len(s)-1):
-                j = query + 1
-            else :
-                j = query
-            while ((s[i] != s[query]) and (s[j] != s[query])):
-                if i > 0:
-                    i -= 1
-                if j < (len(s)-1):
-                    j += 1
-                if (i == 0) and (j == (len(s)-1)):
-                    break
-            print(i, s[i])
-            print(j, s[j])
-            if ((s[i] == s[query]) and (i != query)):
+                flag = check(s,i,query)
+            if flag:
+                break
+            if j != (l-1):
+                j += 1
+                flag = check(s,j,query)
+            if flag:
+                break
+        if flag != 1:
+            a.append(-1)
+        else :
+            if (s[i] == s[query]) and (i != query) :
                 a.append(i)
-            elif ((s[j] == s[query]) and (j != query)):
+            elif (s[j] == s[query]) and (j != query):
                 a.append(j)
     return a
-
 
 if __name__ == '__main__':
