054140620d84ab45b5385fbb3dccf2ce59ca3da1
Rahul <rahulatrkm@gmail.com>
2018-11-05 23:18:23 +0530

    who's the closet-implemented hash map but still TLE

--- wtc.py
+++ wtc.py
@@ -16,39 +16,49 @@ import sys
 #  1. STRING s
 #  2. INTEGER_ARRAY queries
 #
-def check(s, q, qc):
-    if s[q] == s[qc]:
-        return True
-    else:
-        return False
-
 def closest(s, queries):
     # Write your code here
+    sd = dict()
+    for i,j in enumerate(s):
+        if j in sd:
+            sd[j].append(i)
+        else:
+            sd[j] = [i]
     a = []
     for query in queries:
-        i = query
-        j = query
-        l = len(s)
-        flag = 0
-        if queries.co
-        while (i != 0) or (j != (l-1)):
-            if i != 0:
-                i -= 1
-                flag = check(s,i,query)
-            if flag:
-                break
-            if j != (l-1):
-                j += 1
-                flag = check(s,j,query)
-            if flag:
-                break
-        if flag != 1:
+        temp = sd[s[query]]
+        t1 = temp.index(query)
+        if len(temp) == 1:
             a.append(-1)
-        else :
-            if (s[i] == s[query]) and (i != query) :
-                a.append(i)
-            elif (s[j] == s[query]) and (j != query):
-                a.append(j)
+        else:
+            if t1 == 0:
+                a.append(temp[1])
+            elif t1 == (len(temp)-1):
+                a.append(temp[t1-1])
+            else:
+                if abs(temp[t1-1] - query) <= abs(temp[t1+1] - query):
+                    a.append(temp[t1-1])
+                else:
+                    a.append(temp[t1+1])
+
     return a
 
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = closest(s, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
